06c828a956e9ac22a2200ea1d531dc2990557ae4
Sinanmalik <sinanmalik3113@gmail.com>
2024-02-12 18:44:20 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solution in this file!
+// Define the companyName variable as 'Scuber' and declare it as a constant
+const companyName = 'Scuber';
+
+// Define the mostProfitableNeighborhood variable as 'Chelsea' and declare it using let
+let mostProfitableNeighborhood = 'Chelsea';
+
+// Define the companyCeo variable as 'Susan Smith' and declare it using let
+let companyCeo = 'Susan Smith';
+
+// Export the variables
+module.exports = {
+  companyName,
+  mostProfitableNeighborhood,
+  companyCeo
+};
